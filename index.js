a679d2feabe97a539e227138c39faebf12384fed
pierrebachour <pierre.b@scalizer.fr>
2025-07-16 15:07:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,19 +8,69 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-77d9da56-fe74-4ba1-a83a-ee367b232873';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObjects = 'https://api.hubapi.com/crm/v3/objects/2-144716310?properties=name,sexe,pays';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    
+    try {
+        const resp = await axios.get(customObjects, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { 
+            title: 'Custom Objects | HubSpot APIs', 
+            data 
+        });      
+    } catch (error) {
+        console.error(error);
+        res.render('homepage', { 
+            title: 'Custom Objects | HubSpot APIs', 
+            data: [] 
+        });
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' 
+    });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newObject = {
+        properties: {
+            "name": req.body.name,
+            "sexe": req.body.sexe,
+            "pays": req.body.pays
+        }
+    };
+
+    const createCustomObject = 'https://api.hubapi.com/crm/v3/objects/2-144716310';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(createCustomObject, newObject, { headers });
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+        res.redirect('/update-cobj');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -65,7 +115,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
